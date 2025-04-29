5896b17f0108762b54871aff440930addc1be852
Siddhant Patel <siddhantt10@users.noreply.github.com>
2025-04-30 03:01:27 +0530

    Posting Compatible

--- app/database.py
+++ app/database.py
@@ -2,11 +2,24 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-# db creds
-SQLALCHEMY_DATABASE_URL = settings.SIDD_DB_URL # type: ignore
+from dotenv import load_dotenv
+from pydantic_settings import BaseSettings, SettingsConfigDict
+import os
+
+load_dotenv()
+
+
+class Settings(BaseSettings):
+    DATABASE_URL: str
+
+    model_config = SettingsConfigDict(env_file=".env", env_file_encoding="utf-8")
+
+
+settings = Settings()
+
 
 # Engine & Session factory
-engine = create_engine(SQLALCHEMY_DATABASE_URL, pool_size=10, max_overflow=20)
+engine = create_engine(url=settings.DATABASE_URL, pool_size=10, max_overflow=20)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 # Base class for models
